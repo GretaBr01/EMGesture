11ed953cd6bdf290d906b3104920cdc318398fe9
GretaBr01 <gretabrognoli@gmail.com>
2025-05-09 09:16:15 +0200

    run this script to create the dataset

---
+++ Dataset/MainCreateDataset.py
@@ -0,0 +1,8 @@
+with open("segment_series.py") as f:
+    exec(f.read())
+
+with open("create_df_segments.py") as f:
+    exec(f.read())
+
+with open("create_df_features.py") as f:
+    exec(f.read())
